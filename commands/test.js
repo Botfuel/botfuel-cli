f5cd9eb4f7636e04db695e9a23107348a877ab40
Olivier <leplus.olivier@gmail.com>
2018-09-17 17:25:20 +0200

    fix(test): fuel test now displays colors

--- commands/test.js
+++ commands/test.js
@@ -17,17 +17,10 @@ exports.handler = function (argv) {
   const BOTFUEL_APP_TOKEN = process.env.BOTFUEL_APP_TOKEN || environment.botfuel.appToken;
   const BOTFUEL_APP_ID = process.env.BOTFUEL_APP_ID || environment.botfuel.appId;
   const BOTFUEL_APP_KEY = process.env.BOTFUEL_APP_KEY || environment.botfuel.appKey;
-
-  console.log(`BOTFUEL_APP_TOKEN : ${BOTFUEL_APP_TOKEN}`);
-  console.log(`BOTFUEL_APP_ID : ${BOTFUEL_APP_ID}`);
-  console.log(`BOTFUEL_APP_KEY : ${BOTFUEL_APP_KEY}`);
   
   let command = `BOTFUEL_APP_TOKEN=${BOTFUEL_APP_TOKEN} BOTFUEL_APP_ID=${BOTFUEL_APP_ID} BOTFUEL_APP_KEY=${BOTFUEL_APP_KEY}`;
   
   command = `${command} npm run test`;
-  // if (argv.config) {
-  //   command = `${command} ${argv.config}-config`;
-  // }
   
   console.log(`commant : ${command}`);
   shell.exec(command);
